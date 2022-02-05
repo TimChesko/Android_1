04b34d2740836614da8e8b6b0aec17407a15be9f
Timofey <timofey.lazarev@gmail.com>
2022-02-05 11:43:59 +0300

    Выполнил 1 домашнее задание

--- app/src/main/java/com/example/android1/MainActivity.java
+++ app/src/main/java/com/example/android1/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//CTRL + ALT + O - удаление лишних import
+//CTRL + ALT + L - форматировать
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
