c3d77efe69bf508fd5a4faecc674344af61d2b0c
Timofey <timofey.lazarev@gmail.com>
2022-02-07 00:25:09 +0300

    Домашнее задание 3 Создал объект с данными и операциями Создал горизонтальный макет *Не работает saveInstance

--- app/src/main/java/com/example/android1/MainActivity.java
+++ app/src/main/java/com/example/android1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android1;
 
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,34 +13,44 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     //numbers
-    Button oneB = findViewById(R.id.one);
-    Button twoB = findViewById(R.id.two);
-    Button threeB = findViewById(R.id.three);
-    Button fourB = findViewById(R.id.four);
-    Button fiveB = findViewById(R.id.five);
-    Button sixB = findViewById(R.id.six);
-    Button sevenB = findViewById(R.id.seven);
-    Button eightB = findViewById(R.id.eight);
-    Button nineB = findViewById(R.id.nine);
-    Button zeroB = findViewById(R.id.zero);
+    Button oneB;
+    Button twoB;
+    Button threeB;
+    Button fourB;
+    Button fiveB;
+    Button sixB;
+    Button sevenB;
+    Button eightB;
+    Button nineB;
+    Button zeroB;
     //do
-    Button procentB = findViewById(R.id.procent);
-    Button divideB = findViewById(R.id.divide);
-    Button multiplyB = findViewById(R.id.multiply);
-    Button minusB = findViewById(R.id.minus);
-    Button sumB = findViewById(R.id.sum);
-    //del
-    Button deleteB = findViewById(R.id.delete);
-    Button celB = findViewById(R.id.cel);
+    Button procentB;
+    Button divideB;
+    Button multiplyB;
+    Button minusB;
+    Button sumB ;
+    Button equal;
+    Button dote;
+    //delete
+    Button deleteB;
+    Button celB;
+    //editText - окно ввода
+    EditText editText;
+    String saveText = "";
+    Double num1;
+    String make;
+    //TAG
+    static String TAG = "editText";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
         initListner();
     }
 
-    public void initListner(){
+    private void initListner(){
         oneB.setOnClickListener(listener);
         twoB.setOnClickListener(listener);
         threeB.setOnClickListener(listener);
@@ -57,6 +68,31 @@ public class MainActivity extends AppCompatActivity {
         sumB.setOnClickListener(listener);
         deleteB.setOnClickListener(listener);
         celB.setOnClickListener(listener);
+        equal.setOnClickListener(listener);
+        dote.setOnClickListener(listener);
+    }
+
+    private void initView() {
+        oneB = findViewById(R.id.one);
+        twoB = findViewById(R.id.two);
+        threeB = findViewById(R.id.three);
+        fourB = findViewById(R.id.four);
+        fiveB = findViewById(R.id.five);
+        sixB = findViewById(R.id.six);
+        sevenB = findViewById(R.id.seven);
+        eightB = findViewById(R.id.eight);
+        nineB = findViewById(R.id.nine);
+        zeroB = findViewById(R.id.zero);
+        procentB = findViewById(R.id.procent);
+        divideB = findViewById(R.id.divide);
+        multiplyB = findViewById(R.id.multiply);
+        minusB = findViewById(R.id.minus);
+        sumB = findViewById(R.id.sum);
+        deleteB = findViewById(R.id.delete);
+        celB = findViewById(R.id.cel);
+        editText = findViewById(R.id.editText);
+        equal = findViewById(R.id.equal);
+        dote = findViewById(R.id.dote);
     }
 
     View.OnClickListener listener = new View.OnClickListener() {
@@ -64,40 +100,139 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v){
             switch(v.getId()){
                 case R.id.one:
+                    saveText = saveText+"1";
+                    editText.setText(saveText);
                     break;
                 case R.id.two:
+                    saveText = saveText+"2";
+                    editText.setText(saveText);
                     break;
                 case R.id.three:
+                    saveText = saveText+"3";
+                    editText.setText(saveText);
                     break;
                 case R.id.four:
+                    saveText = saveText+"4";
+                    editText.setText(saveText);
                     break;
                 case R.id.five:
+                    saveText = saveText+"5";
+                    editText.setText(saveText);
                     break;
                 case R.id.six:
+                    saveText = saveText+"6";
+                    editText.setText(saveText);
                     break;
                 case R.id.seven:
+                    saveText = saveText+"7";
+                    editText.setText(saveText);
                     break;
                 case R.id.eight:
+                    saveText = saveText+"8";
+                    editText.setText(saveText);
                     break;
                 case R.id.nine:
+                    saveText = saveText+"9";
+                    editText.setText(saveText);
                     break;
                 case R.id.zero:
+                    saveText = saveText+"0";
+                    editText.setText(saveText);
                     break;
                 case R.id.procent:
+                    num1 = Double.valueOf(saveText);
+                    make = "%";
+                    editText.setText(compile(Double.valueOf(saveText), num1, make));
                     break;
                 case R.id.divide:
+                    num1 = Double.valueOf(saveText);
+                    make = "/";
+                    saveText = "";
+                    editText.setText("");
+                    editText.setHint("На какое число поделить ?");
                     break;
                 case R.id.multiply:
+                    num1 = Double.valueOf(saveText);
+                    make = "*";
+                    saveText = "";
+                    editText.setText("");
+                    editText.setHint("На какое число умножить ?");
                     break;
                 case R.id.minus:
+                    num1 = Double.valueOf(saveText);
+                    make = "-";
+                    saveText = "";
+                    editText.setText("");
+                    editText.setHint("Из какого числа вычесть ?");
                     break;
                 case R.id.sum:
+                    num1 = Double.valueOf(saveText);
+                    make = "+";
+                    saveText = "";
+                    editText.setText("");
+                    editText.setHint("К какому числу прибавить ?");
                     break;
                 case R.id.cel:
+                    make = "";
+                    saveText = "";
+                    editText.setText("");
+                    break;
+                case R.id.equal:
+                    String res = compile(Double.valueOf(saveText), num1, make);
+                    saveText = res;
+                    editText.setText(res);
                     break;
                 case R.id.delete:
-                    break;
+                    saveText = editText.getText().toString();
+                    try {
+                        saveText = saveText.substring(0,saveText.length()-1);
+                        editText.setText(saveText);
+                    } catch (Exception ex){}
+                case R.id.dote:
+                    saveText = saveText+".";
+                    editText.setText(saveText);
             }
         }
+
+        private String compile(Double valueOf, Double num1, String make) {
+            double res = 0;
+            switch(make){
+                case "+":
+                    res = valueOf + num1;
+                    break;
+                case "-":
+                    res = num1 - valueOf;
+                    break;
+                case "*":
+                    res = num1 * valueOf;
+                    break;
+                case "/":
+                    res = num1 / valueOf;
+                    break;
+                case "%":
+                    res = num1 / 100;
+            }
+            return String.valueOf(res);
+        }
     };
+
+    @Override
+    //запомнили
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("saveText", saveText);
+        outState.putString("editText", editText.toString());
+        outState.putString("make", make);
+        outState.putDouble("num1", num1);
+    }
+
+    @Override
+    //восстановили
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        saveText = savedInstanceState.getString(saveText);
+        editText.setText(savedInstanceState.getString(String.valueOf(editText)));
+        make = savedInstanceState.getString(make);
+        num1 = savedInstanceState.getDouble(String.valueOf(num1));
+    }
 }
